12b7bcdb508e2ffbcfa5c42d1846a58c4a59b4b5
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2020-04-16 16:27:26 +0300

    new material component

--- src/components/Learning/Lecturer/LecturerView.js
+++ src/components/Learning/Lecturer/LecturerView.js
@@ -1,58 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { getMaterials } from '../../../Redux/actions';
+import React from 'react';
+import StudyMaterials from '../LecturerAndStudent/StudyMaterials';
+
 import { Link } from 'react-router-dom';
-const LecturerView = ({ getMaterials, materials }) => {
-    const [idChanged, setIdChanged] = useState('');
-
-    //return buttons by the specific id clicked
-    const buttonsCreate = ({ id }) => {
-        if (idChanged === id) {
-            return (
-                <div>
-                    <Link to="/LecturerView/ViewClass" className="ui button primary">View Class</Link>
-                    <button className="ui button primary">View Subjects</button>
-                    <button className="ui button green">Edit</button>
-                    <Link to={`/LecturerView/DeleteMaterial/${id}`} className="ui button red">Delete</Link>
-                </div>
-            )
-        }
-        else return null;
-    }
-
-    const renderList = () => {
-        return materials.map(material => {
-            return (
-                <div className="item" key={material.id}>
-                    <div className="content">
-                        <h3 onClick={() => setIdChanged(material.id)} style={{ color: 'blue' }}>
-                            {material.type}
-                        </h3>
-                        <h6>{material.description}</h6>
-                    </div>
-                    {buttonsCreate(material)}
-                </div >
-            )
-        })
-    }
-    /* when the value in getMaterials is changed useEffect call again to this function,
-         but the value never change that way is call only 1 time, (like componentDidMount) */
-    useEffect(() => { getMaterials(); }, [getMaterials])
-
+const LecturerView = () => {
     return (
-        <div className="ui container" style={{ marginTop: '20px' }}>
-            <div className="ui celled list">
-                <h1 style={{ fontWeight: '700' }}>Materials List - Lecturer</h1><br />
-                {renderList()}
-                <Link to='/LecturerView/NewMaterial' className="ui right floated primary button">Add Metarial</Link>
-                <br /><br /><br />
-            </div>
+        <div className="ui container" >
+            <StudyMaterials userMessage={'Lecturer Mode'}>
+                <Link style={{ marginTop: '-10px' }}
+                    to='/LecturerView/NewMaterial' className="ui right floated primary button">
+                    Add Metarial
+                </Link>
+            </StudyMaterials>
         </div>
     )
 }
-const mapStateToProps = (state) => {
-    return {
-        materials: Object.values(state.materials)
-    }
-}
-export default connect(mapStateToProps, { getMaterials })(LecturerView);
+
+export default LecturerView;
--- src/components/Learning/LecturerAndStudent/StudyMaterials.js
+++ src/components/Learning/LecturerAndStudent/StudyMaterials.js
@@ -2,19 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { getMaterials } from '../../../Redux/actions';
 import { Link } from 'react-router-dom';
-const StudyMaterials = ({ getMaterials, materials }) => {
+const StudyMaterials = ({ getMaterials, materials, children, userMessage }) => {
     const [idChanged, setIdChanged] = useState('');
+    const { isLecturer } = JSON.parse(localStorage.getItem("userCredential"));
 
+    const lecturerButtons = () => {
+        return isLecturer ?
+            <React.Fragment>
+                <button className="ui button red">Edit</button>
+                <Link to={`/LecturerView/DeleteMaterial/${idChanged}`} className="ui button red">Delete</Link>
+            </React.Fragment> : null;
+    }
     //return buttons by the specific id clicked
     const buttonsCreate = ({ id }) => {
+        const userType = isLecturer ? 'LecturerView' : 'StudentView';
         return idChanged === id ? (
             (
-                <div>
-                    <Link to="/LecturerView/ViewClass" className="ui button primary">View Class</Link>
+                <React.Fragment>
+                    <Link to={`/${userType}/ViewClass`} className="ui button green">View Class</Link>
                     <button className="ui button primary">View Subjects</button>
-                    <button className="ui button green">Edit</button>
-                    <Link to={`/LecturerView/DeleteMaterial/${id}`} className="ui button red">Delete</Link>
-                </div>
+                    {lecturerButtons()}
+                </React.Fragment>
             )
         ) : null;
     }
@@ -24,7 +32,7 @@ const StudyMaterials = ({ getMaterials, materials }) => {
             return (
                 <div className="item" key={material.id}>
                     <div className="content">
-                        <h3 onClick={() => setIdChanged(material.id)} style={{ color: 'blue' }}>
+                        <h3 onClick={() => setIdChanged(material.id)} style={{ color: 'green' }}>
                             {material.type}
                         </h3>
                         <h6>{material.description}</h6>
@@ -41,10 +49,10 @@ const StudyMaterials = ({ getMaterials, materials }) => {
     return (
         <div className="ui container" style={{ marginTop: '20px' }}>
             <div className="ui celled list">
-                <h1 style={{ fontWeight: '700' }}>Materials List - Lecturer</h1><br />
+                <h1 style={{ fontWeight: '700' }}>Materials List - {userMessage}</h1><br />
                 {renderList()}
-                <Link to='/LecturerView/NewMaterial' className="ui right floated primary button">Add Metarial</Link>
-                <br /><br /><br />
+                {children}
+                <br /><br />
             </div>
         </div>
     )
