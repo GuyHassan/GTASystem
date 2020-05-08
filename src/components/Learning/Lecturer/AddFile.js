566ec948dc26bed6bab230865c74733e41de8653
yinonhirary <33252971+yinonhirary@users.noreply.github.com>
2020-05-08 19:27:11 +0300

    added storage bucket

--- src/components/Learning/Lecturer/AddFile.js
+++ src/components/Learning/Lecturer/AddFile.js
@@ -1,13 +1,24 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { server } from '../../../Apis/server';
 const AddFile = () => {
+    const [fileUp, setFileUp] = useState();
     const fileUploadHandler = (event) => {
-        console.log(event.target.files)
+        setFileUp(event.target.files[0]);
+    }
+    const onSubmitFile = () => {
+        const fd = new FormData();
+        
+        fd.append("pdf", fileUp, fileUp.name);
+        console.log(fd)
+        server.post('/uploadFile', fd).then((res) => {
+            console.log(res);
+        })
     }
     return (
         <div>
             <h1>Add File</h1>
             <input type="file" onChange={fileUploadHandler} />
+            <button onClick={onSubmitFile}>Submit file</button>
         </div>
     )
 }
