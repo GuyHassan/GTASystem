caf612d73e95d1afc67fbbb453b00b9c661bdd3b
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2020-05-25 15:14:31 +0300

    new updates practice and learning material

--- src/components/Learning/Student/MaterialPages.js
+++ src/components/Learning/Student/MaterialPages.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../../Style/MaterialPages.css';
 const MaterialPages = ({ page: { title, freeText, file, streamLink }, numberPage }) => {
     return (
         <div style={{ textAlign: 'center' }}>
@@ -9,7 +9,7 @@ const MaterialPages = ({ page: { title, freeText, file, streamLink }, numberPage
             {file ? <a style={{ marginBottom: '10px' }} className="ui basic green button small" target="_blank" rel="noopener noreferrer" href={file}>Open File</a> : null}
             <br />
 
-            <iframe title="youtube" width="400" height="250vh" src={streamLink} frameBorder="0" allowFullScreen></iframe>
+            <iframe className="frameStream"title="youtube" width="400" height="250vh" src={streamLink} frameBorder="0" allowFullScreen></iframe>
 
         </div >
     )
