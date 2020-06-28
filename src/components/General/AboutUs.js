de3a611f141cd02095c65a5a37ec7abd9588dc06
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2020-06-28 17:56:14 +0300

    testing host

--- src/components/General/AboutUs.js
+++ src/components/General/AboutUs.js
@@ -10,9 +10,9 @@ const AboutUs = () => {
                 Hello, we are Guy Hassan and Yinon Hiriri. <br />
                 Software Engineering students at Sami Shimon College (SCE) fourth year. <br />
                 After four intense years, we reached the final year of the degree and were asked to produce a final project as part of the degree assignments.</h5>
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=guy.hasan@gmail.com" target="_blank" >
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=guy.hasan@gmail.com" target="_blank" rel="noopener noreferrer" >
                 Contact Us :
-                <img src="https://img.icons8.com/cute-clipart/128/000000/google-logo.png" alt="imageDestroyd"/>
+                <img src="https://img.icons8.com/cute-clipart/128/000000/google-logo.png" alt="imageDestroyd" />
             </a>
         </div>
     )
