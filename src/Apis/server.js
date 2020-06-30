80ad4ff896b8eadc2baac95eeec12118eb1bbf4e
yinonhi <yinonhi8102@gmail.com>
2020-06-30 13:17:58 +0300

    test server

--- src/Apis/server.js
+++ src/Apis/server.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 05236a05787901a3221c80d236c69ec516d9ff2f
 const server = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
 });
