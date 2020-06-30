a4fd446aa4ee6913929cd615b5a6d9e7786375ed
yinonhi <yinonhi8102@gmail.com>
2020-06-30 13:12:53 +0300

    New pushing server

--- src/Apis/server.js
+++ src/Apis/server.js
@@ -1,19 +1,9 @@
 import axios from 'axios';
 
-// export default axios.create({
-//     baseURL: 
-// });
-
 
 const server = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
 });
-
-// const server = axios.create({
-//   baseURL: 'http://localhost:3005'
-// });
-
-// process.env.REACT_APP_LOCALHOST_RESTFUL 
 
 
 export { server };
